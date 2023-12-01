c888c7193602b1090bab4e3734e682d89559310b
FrancoSassi03 <francosassi03@gmail.com>
2023-12-01 16:33:49 -0300

    update orders
    ...

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const orderController = require("../controllers/orderController");
+const { expressjwt: checkJwt } = require("express-jwt");
 
 router.get("/", orderController.index);
 router.get("/:id", orderController.show);
-router.post("/", orderController.store);
+router.post("/",checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }), orderController.store);
 router.patch("/:id", orderController.update);
 router.delete("/:id", orderController.destroy);
 
