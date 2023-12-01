41a0c685d8902786913aa53ee4f3a4b600adea7e
Pandemijo <102113551+Pandemijo@users.noreply.github.com>
2023-12-01 15:41:04 -0300

    adminController, adminRoutes

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,22 +1,119 @@
 const Product = require("../models/Product");
 const Admin = require("../models/Admin");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // Display a listing of the resource.
 async function index(req, res) {
-  const admins = await Admin.find();
-  return res.json(admins);
+  try {
+    const products = await Product.find();
+    const admins = await Admin.find();
+    return res.json({ products, admins });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+}
+
+// Show a specified resource.
+async function show(req, res) {
+  try {
+    const { id } = req.params;
+    const admin = await Admin.findById(id);
+
+    if (admin) {
+      return res.json({ admin });
+    } else {
+      return res.status(404).json({ error: "Administrador no encontrado" });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Error interno del servidor" });
+  }
 }
 
 // Store a newly created resource in storage.
-async function store(req, res) {}
+async function store(req, res) {
+  try {
+    const { firstname, lastname, email, password } = req.body;
+
+    if (!firstname || !lastname || !email || !password) {
+      return res.json({ msg: "All fields are required" });
+    }
+    const existingAdmin = await Admin.findOne({ email });
+    if (existingAdmin) {
+      return res.json({ msg: "Admin with this e-mail already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newAdmin = await Admin.create({
+      firstname,
+      lastname,
+      email,
+      password: hashedPassword,
+    });
+
+    const { _id } = newAdmin;
+    const token = jwt.sign({ sub: _id }, process.env.JWT_SECRET);
+
+    return res.json({ token, firstname, lastname, email, id: _id });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+}
 
 // Update the specified resource in storage.
-async function update(req, res) {}
+try {
+  const adminToUpdate = await Admin.findById(req.params.id);
+  let admin;
+    admin = await Admin.findById(req.auth.sub);
+    !admin && (admin = await Admin.findById(req.auth.sub));
+    if (!admin) return res.json({ msg: "Not logged in" });
+    
+    const { firstname, lastname, email, password, newPassword } = req.body;
+    const verifyPassword = await bcrypt.compare(password, adminToUpdate.password);
+    const hashedPassword = newPassword ? await bcrypt.hash(newPassword, 10) : undefined;
+
+    if (password && verifyPassword) {
+      await Admin.findByIdAndUpdate(req.params.id, {
+        firstname,
+        lastname,
+        email,
+        password: hashedPassword,
+      });
+    } else {
+      await Admin.findByIdAndUpdate(req.params.id, {
+        firstname,
+        lastname,
+        email,
+      });
+    }
+    return res.json(adminToUpdate);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
 
 // Remove the specified resource from storage.
-async function destroy(req, res) {}
+async function destroy(req, res) {
+  try {
+    const { id } = req.params;
+    const deletedAdmin = await Admin.findByIdAndDelete(id);
 
-// Otros handlers...
+    if (deletedAdmin) {
+      return res.json({ message: "Administrador eliminado exitosamente" });
+    } else {
+      return res.status(404).json({ error: "Administrador no encontrado" });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Error interno del servidor" });
+  }
+}
+
+
 // ...
 
 module.exports = {
@@ -24,4 +121,5 @@ module.exports = {
   store,
   update,
   destroy,
+  show,
 };
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const adminController = require("../controllers/adminController");
+const { expressjwt: checkJwt } = require("express-jwt");
 
-router.get("/", adminController.index);
-router.post("/", adminController.store);
-router.patch("/:id", adminController.update);
-router.delete("/:id", adminController.destroy);
+const isAdmin = (req, res, next) => {
+//.....
+  const isAdminUser = true; 
+  if (isAdminUser) {
+    next();
+  } else {
+    res.status(403).json({ message: "Acceso denegado. No eres un administrador." });
+  }
+};
+
+router.get("/", isAdmin, adminController.index);
+router.get("/:id", isAdmin, adminController.show);
+router.post("/", isAdmin, adminController.store);
+router.patch("/:id", isAdmin, checkJwt({ secret: process.env.JWT_SECRET, algorithms: ["HS256"] }), adminController.update);
+router.delete("/:id", isAdmin, adminController.destroy);
 
 module.exports = router;
