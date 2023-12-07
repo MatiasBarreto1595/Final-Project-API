0f4fb7a70a6077aa03c68f15836abe11ec766ee0
emzi16 <emilianozapata0638@gmail.com>
2023-12-07 16:22:29 -0300

    Update AdminSeeder.js

--- seeders/AdminSeeder.js
+++ seeders/AdminSeeder.js
@@ -10,7 +10,15 @@ module.exports = async () => {
     email: "laroca@laroca.laroca",
     password: hashedPassword,
   });
+
+  const newAdmin2 = new Admin({
+    firstname: "LeBron",
+    lastname: "James",
+    email: "LeBron@Raymone.James",
+    password: hashedPassword,
+  });
   adminSeeder.push(newAdmin);
+  adminSeeder.push(newAdmin2);
   await Admin.insertMany(adminSeeder);
   console.log("[Database] Se corrió el seeder de Administrador.");
 };
