f850029a08efead336932c58f5772c3eec5d3145
emzi16 <emilianozapata0638@gmail.com>
2023-12-12 12:18:07 -0300

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,11 +1,14 @@
 const { mongoose, Schema } = require("../db");
 
-const orderSchema = new Schema({
-  buyer: { type: Schema.Types.ObjectId, ref: "Buyer" },
-  items: Array,
-  state: String,
-  totalValue : Number,
-},{ timestamps: true });
+const orderSchema = new Schema(
+  {
+    buyer: { type: Schema.Types.ObjectId, ref: "Buyer" },
+    items: Array,
+    state: String,
+    totalValue: Number,
+  },
+  { timestamps: true },
+);
 
 const Order = mongoose.model("Order", orderSchema);
 
